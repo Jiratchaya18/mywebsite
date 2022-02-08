6ae72e9b71bd8579aeb25d63135412815fd40259
Jiratchaya18 <97937708+Jiratchaya18@users.noreply.github.com>
2022-02-08 15:22:30 +0700

    Update table.php

--- table.php
+++ table.php
@@ -17,12 +17,7 @@
         <td>กิต</td>
         <td><img src="https://pbs.twimg.com/profile_images/1285250959866642446/fhGudByH_400x400.jpg"1px"></td>
         <td><a href="https://twitter.com/kittyumbs">twitter</a></td>
-<html>
-  <head>
-    <title>ตารางของฉัน</title>
-  </head>
-  <body>
-    <table border="1">
+
       <tr>
         <th>ลำดับ</th>
         <th>ชื่อ - นามสกุล</th>
@@ -36,12 +31,7 @@
         <td>ตูน</td>
         <td><img src="https://pbs.twimg.com/profile_images/1480571616752922625/CVs_iWoB_400x400.jpg"1px"></td>
         <td><a href="https://twitter.com/Toonthreemando1">twitter</a></td>
-<html>
-  <head>
-    <title>ตารางของฉัน</title>
-  </head>
-  <body>
-    <table border="1">
+
       <tr>
         <th>ลำดับ</th>
         <th>ชื่อ - นามสกุล</th>
