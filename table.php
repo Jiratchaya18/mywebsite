52e6f49e074e760d5f1f7f10d10ba05695c2b29b
Jiratchaya18 <97937708+Jiratchaya18@users.noreply.github.com>
2022-02-08 15:09:57 +0700

    Update table.php

--- table.php
+++ table.php
@@ -32,7 +32,7 @@
       </tr>
       <tr>
         <td>1</td>
-        <td>กฤตย์ จีรพัฒนานุวงศ์</td>
-        <td>กิต</td>
-        <td><img src="https://i.pinimg.com/originals/6a/59/d8/6a59d8837bcf2830c68fcf3a2192574a.jpg"1px"></td>
-        <td><a href="https://twitter.com/kittyumbs">twitter</a></td>         
+        <td>พีรพล เอี่ยมจำรัส</td>
+        <td>ตูน</td>
+        <td><img src="https://pbs.twimg.com/profile_images/1480571616752922625/CVs_iWoB_400x400.jpg"1px"></td>
+        <td><a href="https://twitter.com/Toonthreemando1">twitter</a></td>         
