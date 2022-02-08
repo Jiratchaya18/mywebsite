e7f36f1e65122eef1e9f5d790b63813ac1aabaf0
Jiratchaya18 <97937708+Jiratchaya18@users.noreply.github.com>
2022-02-08 15:19:01 +0700

    Update table.php

--- table.php
+++ table.php
@@ -35,4 +35,24 @@
         <td>พีรพล เอี่ยมจำรัส</td>
         <td>ตูน</td>
         <td><img src="https://pbs.twimg.com/profile_images/1480571616752922625/CVs_iWoB_400x400.jpg"1px"></td>
-        <td><a href="https://twitter.com/Toonthreemando1">twitter</a></td>         
+        <td><a href="https://twitter.com/Toonthreemando1">twitter</a></td>
+<html>
+  <head>
+    <title>ตารางของฉัน</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ - นามสกุล</th>
+        <th>ชื่อเล่น</th>
+        <th>รูป</th>
+        <th>ทวิตเตอร์</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>อนุโรจน์ เกตุเลขา</td>
+        <td>เติร์ด</td>
+        <td><img src="https://pbs.twimg.com/media/FK-3DU5VcAIP3Kp?format=jpg&name=large"1px"></td>
+        <td><a href="https://twitter.com/2nd__4th">twitter</a></td>         
+        
