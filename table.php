49e344427139fb5c3785b4adf5e98f1c9d66e3dd
Jiratchaya18 <97937708+Jiratchaya18@users.noreply.github.com>
2022-02-08 15:06:32 +0700

    Update table.php

--- table.php
+++ table.php
@@ -17,4 +17,22 @@
         <td>กิต</td>
         <td><img src="https://i.pinimg.com/originals/6a/59/d8/6a59d8837bcf2830c68fcf3a2192574a.jpg"1px"></td>
         <td><a href="https://twitter.com/kittyumbs">twitter</a></td>
-         
+<html>
+  <head>
+    <title>ตารางของฉัน</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ - นามสกุล</th>
+        <th>ชื่อเล่น</th>
+        <th>รูป</th>
+        <th>ทวิตเตอร์</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>กฤตย์ จีรพัฒนานุวงศ์</td>
+        <td>กิต</td>
+        <td><img src="https://i.pinimg.com/originals/6a/59/d8/6a59d8837bcf2830c68fcf3a2192574a.jpg"1px"></td>
+        <td><a href="https://twitter.com/kittyumbs">twitter</a></td>         
