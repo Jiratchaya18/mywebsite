694c82afc99b245186e02a35ececa15d09088003
Jiratchaya18 <97937708+Jiratchaya18@users.noreply.github.com>
2022-02-08 14:41:37 +0700

    Update table.php

--- table.php
+++ table.php
@@ -8,12 +8,12 @@
         <th>ลำดับ</th>
         <th>ชื่อ - นามสกุล</th>
         <th>ชื่อเล่น</th>
+        <th>รูป</th>
+        <th>ทวิตเตอร์</th>
       </tr>
       <tr>
         <td>1</td>
-        <td>ยิ่งคุณ รอดทิม</td>
-        <td>ขม</td>
-      </tr>  
-    </table>
-  </body> 
-</html>
+        <td>กฤตย์ จีรพัฒนานุวงศ์</td>
+        <td>กิต</td>
+        <td><img src="คุณกิต.jpg" width="130px"></td>
+        <td><a href="https://twitter.com/kittyumbs">twitter</a></td>
