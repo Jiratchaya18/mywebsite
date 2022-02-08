996f115e7de1863bccf55cb89a7b2594e47aeaa4
Jiratchaya18 <97937708+Jiratchaya18@users.noreply.github.com>
2022-02-08 14:56:20 +0700

    Update table.php

--- table.php
+++ table.php
@@ -15,6 +15,6 @@
         <td>1</td>
         <td>กฤตย์ จีรพัฒนานุวงศ์</td>
         <td>กิต</td>
-        <td><img src="https://i.pinimg.com/originals/11/22/c8/1122c86609e09586acecc3a0d26fb036.jpg"50px"></td>
+        <td><img src="https://www.tvpoolonline.com/wp-content/uploads/2020/12/S__2318355.jpg"50px"></td>
         <td><a href="https://twitter.com/kittyumbs">twitter</a></td>
          
