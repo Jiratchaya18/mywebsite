ea603d223b60230196ab18f0749b14672e4deaf2
Jiratchaya18 <97937708+Jiratchaya18@users.noreply.github.com>
2022-02-18 09:49:34 +0700

    Update page2.php

--- page2.php
+++ page2.php
@@ -1,9 +1,25 @@
 <html>
 	<head>
 		<title>การตกแต่งเว็บไซต์ด้วย CSS</title>
+		<style>
+			#box1{
+				width: 800px;
+				height: 350px;
+			}
+			.box{
+				background-color: black;
+			}
+                        body{
+                                background-color: #FF3333;
+			}
+                        h1{
+                                color: #FFDAB9;
+			}
+		</style>
 	</head>
 	<body>
-		<p style="color:green;">ข้อความนี้สีเขียวปกติ</p>
-		<p style="color:#82933f;">ข้อความนี้สีเขียวรหัสสี</p>
+		<div id="box1" class="box">
+			<h1> น.ส.จิรัชญา ขันแก้ว เลขที่ 18 ม.4/13</h1>
+		</div>
 	</body>
 </html>
