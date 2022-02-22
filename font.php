bfcbfbb385a461f2a4feb0cc27d3cf9242371566
Jiratchaya18 <97937708+Jiratchaya18@users.noreply.github.com>
2022-02-22 20:39:55 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>กำหนดรูปแบบตัวอักษร</title>
+  <style>
+    @font-face{
+      font-family: myFont1;
+      src: url("DM Weekend Regular (1).woff")
+    }
+    @font-face{
+      font-family: myFont2;
+      src: url("smile Regular-1 (2).woff")
+    }
+    #font1{
+	    font-family: myFont1; 
+	  }
+    #font2{
+	font-family: myFont2;    
+    }
+  </style>
+</head>
+<body>
+  <h1 id="font1">นางสาวจิรัชญา ขันแก้ว ม.4/13 เลขที่ 18 เลขประจำตัว 57029</h1>
+  <h1 id="font2">นางสาวจิรัชญา ขันแก้ว ม.4/13 เลขที่ 18 เลขประจำตัว 57029</h1>
+</body>
+</html>
